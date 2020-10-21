c391670997b3a41de74b76906287b49d10126c6d
Chaluemchat Chompuwanichakul <chaluemchat@gmail.com>
2020-10-21 15:36:47 +0700

    Updates

---
+++ precache-manifest.41b1cc73a8de3a045d2f541822832c48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd83786503246e6211fb03f9e0d9d73",
+    "url": "/ChuckNorris/index.html"
+  },
+  {
+    "revision": "472a29ffe6a8517f3aef",
+    "url": "/ChuckNorris/static/css/main.a7cd1164.chunk.css"
+  },
+  {
+    "revision": "17fa09fcab72ea48e17c",
+    "url": "/ChuckNorris/static/js/2.f442d3d9.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/ChuckNorris/static/js/2.f442d3d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "472a29ffe6a8517f3aef",
+    "url": "/ChuckNorris/static/js/main.084ba55f.chunk.js"
+  },
+  {
+    "revision": "c3424ea78205bfb3d990",
+    "url": "/ChuckNorris/static/js/runtime-main.6404d133.js"
+  }
+]);
