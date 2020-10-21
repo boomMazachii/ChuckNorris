4be0caac5a1bd44a584db1ba18da380c44f59677
Chaluemchat Chompuwanichakul <chaluemchat@gmail.com>
2020-10-21 15:35:39 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [jokes, setJokes] = useState([]);
+  const [jokesToShow, setJokesToShow] = useState([]);
+  const [numberResult, setNumberResult] = useState([]);
+  const [firstName,setFirstname] = useState('Chuck');
+  const [lastName,setLastname] = useState('Norris');
+
+  useEffect(() => {
+    fetchAndSetJokes()
+  }, []);
+
+  const  fetchAndSetJokes = () => {
+    fetch(`https://api.icndb.com/jokes?firstName=${firstName}&lastName=${lastName}`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setJokes(res.value);
+        setJokesToShow(res.value.slice(0,numberResult));
+      })
+      .catch((err) => console.log(err));
+  };
+
+
+  const changeName = (e) =>{
+    e.preventDefault();
+      if(firstName === ''|| lastName === '') return
+      fetchAndSetJokes()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class = "navbar"><h1>Chuck Norris</h1></div>
+      <form onSubmit = {changeName} noValidates>
+        <div class = "cards">
+        
+        <div class="field">
+          <label class="label">Number of Result</label>
+            <div class="control">
+              <div class ="n">
+              <input class="input" type="text" placeholder="Number" onChange = {e => setNumberResult(e.target.value)}/>
+              </div>
+            </div>
+          </div>
+
+        <div class="field">
+          <label class="label">Firstname</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="Firstname" onChange = {e => setFirstname(e.target.value)}/>
+            </div>
+          </div>
+
+        <div class="field">
+          <label class="label">Lastname</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="Lastname" onChange = {e => setLastname(e.target.value)}/>
+            </div>
+        </div>
+        
+        <div class="buttons">
+          <button class="button is-primary" type="submit">Submit</button>
+        </div>
+        </div>
+       
+    </form>
+      <div class = "card-content" key = {jokes.id}>
+        {jokesToShow.map(joke => (
+          <div class= "card">{joke.joke}</div>
+        ))}
+      </div>
     </div>
   );
 }
